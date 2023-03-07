ff507f2b1c80f4f303eddcfe3d3e6e564dee656c
Ignelis91 <110096365+Ignelis91@users.noreply.github.com>
2023-03-07 20:02:12 +0200

    Update fxmanifest.lua

--- ig-hunting/fxmanifest.lua
+++ ig-hunting/fxmanifest.lua
@@ -3,10 +3,8 @@ game 'gta5'
 lua54 'yes'
 
 author 'Ignelis'
-description "Gaidy Negausi Nieko"
 
 shared_scripts {
-    '@ig-tokens/init.lua',
     '@es_extended/imports.lua',
     '@ox_lib/init.lua',
     'config.lua'
